831a8c72fdae7e24a95c1a714ebcf747dfb2d39b
yusufanilakduygu <yusufanilakduygu@gmail.com>
2017-09-13 08:54:56 +0300

    Add files via upload

---
+++ dbhack_mssqlModules.py
@@ -0,0 +1,35 @@
+from dbhack_parser import *
+import socket
+import itertools
+ 
+
+def return_string_between(p_data,p_begin,p_end):
+	return p_data[p_data.index(p_begin)+len(p_begin) : p_data.find(p_end, p_data.index(p_begin)+len(p_begin)  )]
+
+
+def mssql_chk(p_server):
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(13)
+
+    server_address = (p_server, 1434)
+    message =  bytearray ([0x03])
+
+    try:
+        sent = sock.sendto(message, server_address)
+
+        data, server = sock.recvfrom(4096)
+    except Exception as error:
+        print ("An exception was thrown!")
+        print (str(error))
+        return
+     
+
+    str_data=str(data)
+    print() 
+    print( "Server Name   :", return_string_between(str_data,"ServerName;",";"))
+    print( "Instance Name :", return_string_between(str_data,"InstanceName;",";"))
+    print( "Version       :", return_string_between(str_data,"Version;",";"))
+    print( "Port Number   :", return_string_between(str_data,"tcp;",";"))
+    print()
+    return
